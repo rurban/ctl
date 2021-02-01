d9a19f0cd59c4c0cb17dfd1643c74dac35c6c380
Reini Urban <rurban@cpan.org>
2021-02-01 09:13:49 +0100

    missed perf_set_clear.c sources

---
+++ tests/perf/set/perf_set_clear.c
@@ -0,0 +1,27 @@
+#include "../../test.h"
+
+#define POD
+#define T int
+#include <ctl/set.h>
+
+#include <time.h>
+
+static int compare(int* a, int* b) { return *a == *b ? 0 : *a < *b ? -1 : 1; }
+
+int main(void)
+{
+    puts(__FILE__);
+    srand(0xbeef);
+    for(int run = 0; run < TEST_PERF_RUNS; run++)
+    {
+        set_int c = set_int_init(compare);
+        int elems = TEST_PERF_CHUNKS * run;
+        for(int elem = 0; elem < elems; elem++)
+            set_int_insert(&c, rand() % elems);
+        long t0 = TEST_TIME();
+        set_int_clear(&c);
+        long t1 = TEST_TIME();
+        printf("%10d %10ld\n", elems, t1 - t0);
+        set_int_free(&c);
+    }
+}
---
+++ tests/perf/set/perf_set_clear.cc
@@ -0,0 +1,21 @@
+#include "../../test.h"
+
+#include <set>
+#include <time.h>
+
+int main()
+{
+    puts(__FILE__);
+    srand(0xbeef);
+    for(int run = 0; run < TEST_PERF_RUNS; run++)
+    {
+        std::set<int> c;
+        int elems = TEST_PERF_CHUNKS * run;
+        for(int elem = 0; elem < elems; elem++)
+            c.insert(rand() % elems);
+        long t0 = TEST_TIME();
+        c.clear();
+        long t1 = TEST_TIME();
+        printf("%10d %10ld\n", elems, t1 - t0);
+    }
+}
